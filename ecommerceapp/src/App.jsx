808294686f46dd6280caef866dfe04783b4433be
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2022-12-31 01:31:31 +0100

    ... created navbar component

--- ecommerceapp/src/App.jsx
+++ ecommerceapp/src/App.jsx
@@ -2,19 +2,34 @@ import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Products from './pages/Products/Products';
 import Product from './pages/Product/Product';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+
+const Layout = () => {
+  return (
+    <div className=''>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
-  },
-  {
-    path: '/products/:id',
-    element: <Products />,
-  },
-  {
-    path: '/product/:id',
-    element: <Product />,
+    element: <Layout />,
+    children: [
+      { path: '/', element: <Home /> },
+      {
+        path: '/products/:id',
+        element: <Products />,
+      },
+      {
+        path: '/product/:id',
+        element: <Product />,
+      },
+    ],
   },
 ]);
 
---
+++ ecommerceapp/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import SearchIcon from '@mui/icons-material/Search';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+  return (
+    <div className='h-20 text-lg'>
+      <div className='flex justify-between py2.5 px-7 items-center'>
+        <div className='flex items-center gap-6'>
+          <div className='flex items-center'>
+            <img
+              src='/img/en.flag.png'
+              className='w-8 h-5'
+              alt='english flag'
+            />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className='flex items-center'>
+            <span>USD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className='flex items-center'>
+            <Link to='/products/1'>Women</Link>
+          </div>
+          <div className='flex items-center'>
+            <Link to='/products/2'>Men</Link>
+          </div>
+          <div className='flex items-center tracking-wide'>
+            <Link to='/products/3'>Children</Link>
+          </div>
+        </div>
+        <div className='text-3xl'>
+          <Link to='/'>LAMASTORE</Link>
+        </div>
+        <div className='flex items-center gap-6'>
+          <div className='flex items-center'>
+            <Link to='/'>Homepage</Link>
+          </div>
+          <div className='flex items-center'>
+            <Link to='/'>About</Link>
+          </div>
+          <div className='flex items-center'>
+            <Link to='/'>Contact</Link>
+          </div>
+          <div className='flex items-center'>
+            <Link to='/'>Stores</Link>
+          </div>
+          <div className='flex items-center gap-4 text-slate-700 cursor-pointer'>
+            <SearchIcon />
+            <PersonOutlineOutlinedIcon />
+            <FavoriteBorderOutlinedIcon />
+            <div className='relative'>
+              <ShoppingCartOutlinedIcon />
+              <span className='text-xs w-4 h-4 rounded-full bg-[#2879fe] text-white absolute flex items-center justify-center -right-1 -top-1'>
+                0
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
